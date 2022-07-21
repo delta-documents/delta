2488e48633bd0542f3e1dd55ef33a75cbd5bbef6
Vadim Tsvetkov <florius0@ninsar.pro>
2022-07-21 20:15:11 +0300

    Implement collection logic

--- lib/delta/collection.ex
+++ lib/delta/collection.ex
@@ -3,15 +3,70 @@ defmodule Delta.Collection do
   defstruct [:id, :name]
   use Delta.Storage.MnesiaHelper, struct: Delta.Collection
 
+  alias Delta.Collection.MnesiaHelper
+
   def new(name \\ "unnamed_collection", id \\ UUID.uuid4()) do
     %__MODULE__{id: id, name: name}
   end
 
   def validate(%__MODULE__{id: id, name: name}) do
-    with {:ok, id} <- Delta.Validators.uuid(id, "#{__MODULE__}.id") do
+    with {:ok, id} <- Delta.Validators.uuid(id, "#{inspect(__MODULE__)}.id") do
       {:ok, %__MODULE__{id: id, name: name}}
     end
   end
 
-  def validate(_), do: {:error, "Not an instance of #{__MODULE__}"}
+  def validate(_), do: {:error, "Not an instance of #{inspect(__MODULE__)}"}
+
+  def list do
+    :mnesia.transaction(fn -> MnesiaHelper.list() end)
+  end
+
+  def get(m) do
+    :mnesia.transaction(fn ->
+      with [r] <- MnesiaHelper.get(m) do
+        r
+      else
+        [] -> :mnesia.abort("#{inspect(__MODULE__)} with id = #{m.id} does not exist")
+      end
+    end)
+  end
+
+  def create(m) do
+    :mnesia.transaction(fn ->
+      with {:get, []} <- MnesiaHelper.get(m),
+           {:validate, {:ok, m}} <- {:validate, validate(m)} do
+        MnesiaHelper.write(m)
+      else
+        {:get, [_]} -> :mnesia.abort("#{inspect(__MODULE__)} with id = #{m.id} does not exist")
+        {:validate, {:error, err}} -> :mnesia.abort(err)
+      end
+    end)
+  end
+
+  def update(m) do
+    :mnesia.transaction(fn ->
+      with {:get, [_]} <- MnesiaHelper.get(m),
+           {:validate, {:ok, m}} <- {:validate, validate(m)} do
+        MnesiaHelper.write(m)
+      else
+        {:get, []} -> :mnesia.abort("#{inspect(__MODULE__)} with id = #{m.id} already exists")
+        {:validate, {:error, err}} -> :mnesia.abort(err)
+      end
+    end)
+  end
+
+  def update(m, attrs), do: update(struct(m, attrs))
+
+  def delete(m) do
+    :mnesia.transaction(fn ->
+      MnesiaHelper.delete(m)
+    end)
+  end
+
+  def collection_id(id) do
+    case MnesiaHelper.get(id) do
+      [%{id: id}] -> [id]
+      x -> x
+    end
+  end
 end
