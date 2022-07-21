754dda6d2bdcb0432cc3893b7f094cebf22b144a
Vadim Tsvetkov <florius0@ninsar.pro>
2022-07-21 16:23:55 +0300

    Avoid transaction nesting

--- lib/delta/storage/mnesia_helper.ex
+++ lib/delta/storage/mnesia_helper.ex
@@ -4,35 +4,29 @@ defmodule Delta.Storage.MnesiaHelper do
       def list, do: foldl([], &[&1 | &2])
 
       def foldl(acc0, fun) do
-        :mnesia.transaction(fn ->
-          :mnesia.foldl(fn r, acc -> fun.(from_record(r), acc) end, acc0, __MODULE__)
-        end)
+        :mnesia.foldl(fn r, acc -> fun.(from_record(r), acc) end, acc0, __MODULE__)
       end
 
       def foldr(acc0, fun) do
-        :mnesia.transaction(fn ->
-          :mnesia.foldr(fn r, acc -> fun.(from_record(r), acc) end, acc0, __MODULE__)
-        end)
+        :mnesia.foldr(fn r, acc -> fun.(from_record(r), acc) end, acc0, __MODULE__)
       end
 
       def get(%__MODULE__{id: id}), do: get(id)
 
       def get(id) do
-        :mnesia.transaction(fn ->
-          [r] = :mnesia.read(__MODULE__, id)
-          from_record(r)
-        end)
+        [r] = :mnesia.read(__MODULE__, id)
+        from_record(r)
       end
 
       def write(%__MODULE__{} = m) do
-        :mnesia.transaction(fn -> :mnesia.write(to_record(m)) end)
+        :mnesia.write(to_record(m))
       end
 
       def delete(%__MODULE__{id: id}) do
-        :mnesia.transaction(fn -> :mnesia.delete({__MODULE__, id}) end)
+        :mnesia.delete({__MODULE__, id})
       end
 
-      defoverridable([list: 0, foldl: 2, foldr: 2, get: 1, write: 1, delete: 1])
+      defoverridable(list: 0, foldl: 2, foldr: 2, get: 1, write: 1, delete: 1)
     end
   end
 
