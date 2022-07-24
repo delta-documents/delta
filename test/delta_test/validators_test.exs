f17db2941cfd13ced353865f8730dbaa31ad3eb6
Vadim Tsvetkov <florius0@ninsar.pro>
2022-07-24 19:06:50 +0300

    Rename casess in Delta.Validators tests

--- test/delta_test/validators_test.exs
+++ test/delta_test/validators_test.exs
@@ -2,7 +2,7 @@ defmodule DeltaTest.ValidatorsTest do
   use ExUnit.Case
   alias Delta.Validators
 
-  test "Validators.uuid/2 validates uuid to be default and v4" do
+  test "Delta.Validators.uuid/2 validates uuid to be default and v4" do
     u = UUID.uuid4()
     assert {:ok, ^u} = Validators.uuid(u)
 
@@ -12,27 +12,27 @@ defmodule DeltaTest.ValidatorsTest do
     assert {:error, _} = Validators.uuid("not_an_uuid")
   end
 
-  test "Validators.maybe_uuid/2 validates uuid to be default and v4 or nil" do
+  test "Delta.Validators.maybe_uuid/2 validates uuid to be default and v4 or nil" do
     assert {:ok, _} = Validators.maybe_uuid(UUID.uuid4())
     assert {:ok, _} = Validators.maybe_uuid(nil)
 
     assert {:error, %{got: "UUID of type hex"}} = Validators.maybe_uuid(UUID.uuid4(:hex))
   end
 
-  test "Validators.map/2" do
+  test "Delta.Validators.map/2" do
     assert {:ok, %{}} = Validators.map(%{})
 
     assert {:error, %{expected: "a map", got: "[]"}} = Validators.map([])
   end
 
-  test "Validators.path/2" do
+  test "Delta.Validators.path/2" do
     assert {:ok, ["a", "b", 1]} = Validators.path(["a", "b", 1])
 
     assert {:error, %{got: "nil"}} = Validators.path(nil)
     assert {:error, %{got: "[1, %{}]"}} = Validators.path([1, %{}])
   end
 
-  test "Validators.kind/2" do
+  test "Delta.Validators.kind/2" do
     assert {:ok, :add} = Validators.kind(:add)
     assert {:ok, :update} = Validators.kind(:update)
     assert {:ok, :remove} = Validators.kind(:remove)
