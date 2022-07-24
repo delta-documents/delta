bfc7215f4a804238ec143a90aa2209472756ad37
Vadim Tsvetkov <florius0@ninsar.pro>
2022-07-24 19:07:02 +0300

    Add Delta.Storage.RecordHelper tests

---
+++ test/delta_test/storage_test.exs
@@ -0,0 +1,16 @@
+defmodule DeltaTest.StorageTest do
+  use ExUnit.Case
+  use Delta.Storage.RecordHelper
+
+  alias __MODULE__, as: S
+
+  defstruct [:a, b: 2]
+
+  test "to_record/1" do
+    assert {S, 1, 2} == to_record(%S{a: 1, b: 2})
+  end
+
+  test "from_record" do
+    assert %S{a: 1, b: 2} == from_record({S, 1, 2})
+  end
+end
