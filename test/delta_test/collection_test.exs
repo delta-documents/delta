752878604f0b324890fdfabd8b0505936b82c55d
Vadim Tsvetkov <florius0@ninsar.pro>
2022-07-24 23:28:34 +0300

    Add Delta. prefix to tests

--- test/delta_test/collection_test.exs
+++ test/delta_test/collection_test.exs
@@ -6,13 +6,13 @@ defmodule DeltaTest.CollectionTest do
 
   alias Delta.Collection
 
-  test "Collection.validate/1" do
+  test "Delta.Collection.validate/1" do
     assert {:ok, _} = collection() |> Collection.validate()
     assert {:error, _} = %Collection{id: "not_an_uuid"} |> Collection.validate()
     assert {:error, _} = "not a collection" |> Collection.validate()
   end
 
-  test "Collection.new/2 generates unique and correct uuid" do
+  test "Delta.Collection.new/2 generates unique and correct uuid" do
     c1 = %{id: id1} = Collection.new()
     c2 = %{id: id2} = Collection.new()
 
@@ -21,7 +21,7 @@ defmodule DeltaTest.CollectionTest do
     assert {:ok, ^c2} = Collection.validate(c2)
   end
 
-  test "Collection.get/1" do
+  test "Delta.Collection.get/1" do
     c = collection()
 
     assert {:aborted, _} = Collection.get_transaction(c)
@@ -32,7 +32,7 @@ defmodule DeltaTest.CollectionTest do
     assert {:atomic, ^c} = Collection.get_transaction(c)
   end
 
-  test "Collection.list/0" do
+  test "Delta.Collection.list/0" do
     c = collection()
 
     assert {:atomic, []} = Collection.list_transaction()
@@ -42,17 +42,17 @@ defmodule DeltaTest.CollectionTest do
     assert {:atomic, [^c]} = Collection.list_transaction()
   end
 
-  test "Collection.create/1 creates collection if one does not exist" do
+  test "Delta.Collection.create/1 creates collection if one does not exist" do
     c = collection()
     assert {:atomic, ^c} = Collection.create_transaction(c)
     assert {:aborted, _} = Collection.create_transaction(c)
   end
 
-  test "Collection.create/1 of invalid collection aborts transaction" do
+  test "Delta.Collection.create/1 of invalid collection aborts transaction" do
     assert {:aborted, _} = Collection.create_transaction(%Collection{id: 123})
   end
 
-  test "Collection.update/2 updates collection if one exists" do
+  test "Delta.Collection.update/2 updates collection if one exists" do
     c = collection()
 
     assert {:aborted, _} = Collection.update_transaction(c, %{name: 123})
@@ -62,7 +62,7 @@ defmodule DeltaTest.CollectionTest do
     assert {:atomic, _} = Collection.update_transaction(c, %{name: 123})
   end
 
-  test "Collection.delete/1 deletes documents and changes cascade" do
+  test "Delta.Collection.delete/1 deletes documents and changes cascade" do
     {:atomic, c} = create_collection()
     {:atomic, d} = create_document()
     {:atomic, m} = create_change()
@@ -74,7 +74,7 @@ defmodule DeltaTest.CollectionTest do
     assert {:aborted, _} = Delta.Change.get_transaction(m)
   end
 
-  test "Collection.delete/1 of non-existing collection is :ok" do
+  test "Delta.Collection.delete/1 of non-existing collection is :ok" do
     assert {:atomic, :ok} = Collection.delete_transaction("123")
   end
 end
