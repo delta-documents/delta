072538c621027b46ef4c847927af13b037a8a9a7
Vadim Tsvetkov <florius0@ninsar.pro>
2022-08-20 23:47:58 +0300

    Add connections mockup

---
+++ lib/delta/connection.ex
@@ -0,0 +1,24 @@
+defmodule Delta.Connection do
+  @moduledoc """
+  Behaviour for connections to Delta.
+  Instances is expected to:
+    - manage connection from user
+    - join Swarm group `Delta.Connection` to be able to recieve notifications
+  """
+
+  @typedoc """
+  Events for clients:
+    - `{:created, document_id}` – `Delta.Document.t()` with `id = document_id` is created
+    - `{:created, document_id}` – `Delta.Document.t()` with `id = document_id` is deleted
+    - `{:added, commit}` – `Delta.Commit.t()` was added
+    - `{:squashed, into, what}` – `Delta.Commit.t()` `what` was squashed `into`
+  """
+  @type event() ::
+          {:created, document_id :: Delta.uuid4()}
+          | {:deleted, document_id :: Delta.uuid4()}
+          # Commit events
+          | {:added, Delta.Commit.t()}
+          | {:squashed, into :: Delta.Commit.t(), Delta.Commit.t()}
+
+  @callback handle_call({:notify, document_id :: Delta.uuid4(), event()}, any(), any()) :: :ok
+end
---
+++ lib/delta/connection/supervisor.ex
@@ -0,0 +1,5 @@
+defmodule Delta.Connection.Supervisor do
+  @moduledoc """
+  Supervises all connection processes
+  """
+end
