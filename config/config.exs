2eda729b49023872700bead668db795d7be520ad
Vadim Tsvetkov <florius0@ninsar.pro>
2022-10-19 20:57:38 +0300

    feat(mongo): add mongo conn params to docs

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ import Config
 
 config :mnesia, dir: './data/#{config_env()}/#{node()}'
 
-import_config "#{config_env()}.exs"
+config :delta, :mongo_params, [
+  database: "delta_#{config_env()}",
+  hostname: "localhost",
+  port: 27017
+]
